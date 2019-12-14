49b0e1b7696bbfdb49610c7eca61320589584749
Ankit Deshpande <ankitd009@users.noreply.github.com>
2019-12-14 17:25:10 +0530

    Add solution for Birthday Cake Candles

---
+++ HackerRank/birthdaycakecandles/Solution.java
@@ -0,0 +1,65 @@
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int findMaxElement(int[] ar){
+        int maxElem = 0;
+        for (int elem : ar){
+            if (elem >= maxElem){
+                maxElem = elem;
+            }
+        }
+        return maxElem;
+    }
+
+    static int findFrequencyOfElem(int[] ar, int elem){
+        int count = 0;
+        for (int e : ar){
+            if (e == elem){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    static int birthdayCakeCandles(int[] ar) {
+        return findFrequencyOfElem(ar, findMaxElement(ar));
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
