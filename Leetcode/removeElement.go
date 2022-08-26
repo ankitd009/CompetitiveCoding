79bc2cbece594ee1cefecb58652f7dc9373d8e25
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-08-26 13:10:10 +0530

    Add removeElement.go

---
+++ Leetcode/removeElement.go
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/remove-element/
+
+func removeElement(nums []int, val int) int {
+	count := 0
+	length := len(nums)
+	for i:=0; i< length-count; i++ {
+		elem := nums[i]
+		if elem == val {
+			count++
+		}
+	}
+
+	if count == 0 || count == length {
+		return length - count
+	}
+
+	count = 0
+
+	for i:=0; i< length-count; i++ {
+		elem := nums[i]
+		if elem == val {
+			count++
+			swap(nums, i, length - count)
+		}
+		if nums[i] == val {
+			i--
+		}
+	}
+	return length- count
+}
+
+func swap(nums []int, srcInd, destInd int){
+	tmp := nums[srcInd]
+	nums[srcInd] = nums[destInd]
+	nums[destInd] = tmp
+}
