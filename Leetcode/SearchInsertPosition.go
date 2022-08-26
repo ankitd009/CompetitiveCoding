8d014387a849ee4266b903f05677c013b4082ee4
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-08-26 16:06:41 +0530

    Add SearchInsertPosition.go

---
+++ Leetcode/SearchInsertPosition.go
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/search-insert-position
+
+func searchInsert(nums []int, target int) int {
+    for i:= 0 ; i < len(nums); i++ {
+        if nums[i] >= target {
+            return i
+        }
+    }
+    return len(nums)
+}
