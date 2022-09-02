2bbb3bd6e371cb1cfb1d8d62af8499fa6c9d66b1
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-09-03 02:13:28 +0530

    Add removeLinkedListElements.go

---
+++ Leetcode/removeLinkedListElements.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/remove-linked-list-elements
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func removeElements(head *ListNode, val int) *ListNode {
+	nList := &ListNode{
+		Next: head,
+	}
+	itr := nList
+
+	for itr != nil {
+		if itr.Next != nil &&  itr.Next.Val == val {
+			itr.Next = itr.Next.Next
+		}else {
+			itr = itr.Next
+		}
+
+	}
+
+	return nList.Next
+}
