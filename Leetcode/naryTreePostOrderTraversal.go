c11340be5af144f6083b998f4b25ea95e4243881
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-09-04 03:27:09 +0530

    Add naryTreePostOrderTraversal.go

---
+++ Leetcode/naryTreePostOrderTraversal.go
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) []int {
+    vals  := make([]int, 0, 0)
+    if root == nil {
+        return vals
+    }
+    tO := &traverseOrder {
+        vals: vals,
+    }
+    traverse(root, tO)
+    return tO.vals
+}
+
+type  traverseOrder struct {
+    vals []int
+}
+
+func traverse(node *Node, order *traverseOrder){
+    if node == nil {
+        return
+    }
+    if node.Children != nil {
+        for _, v := range node.Children {
+            traverse(v, order)    
+        }
+    }
+    order.vals = append(order.vals, node.Val)
+}
