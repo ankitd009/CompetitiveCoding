e25fbef9487730e924820d4c39f37f6626fb6cf9
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-09-05 19:00:44 +0530

    Add pathSum.go

---
+++ Leetcode/pathSum.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/path-sum
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+    if root != nil && root.Left == nil && root.Right == nil && root.Val == targetSum {
+        return true
+    }
+    return hasSum(root, 0, targetSum)
+}
+
+func hasSum(node *TreeNode, curSum, targetSum int)bool{
+    if node == nil {
+        return false
+    }
+    sum := curSum + node.Val
+    if sum == targetSum && node.Left == nil && node.Right == nil {
+         return true
+    }
+    return hasSum(node.Left,sum , targetSum) || hasSum(node.Right, sum, targetSum)
+}
