4092c14fd4733e39234e80e1bf2b26b6a5021b7e
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-09-07 01:44:10 +0530

    happyNumber_test.go fix func signature

--- Leetcode/happyNumber_test.go
+++ Leetcode/happyNumber_test.go
@@ -2,14 +2,14 @@ package leetcode
 
 import "testing"
 
-func TestIsHappySuccess(t *testing){
+func TestIsHappySuccess(t *testing.T){
   res := isHappy(19)
   if !res {
 	  t.Errorf("Expected: true, Got: %v", res)
   }
 }
 
-func TestIsHappyFailure(t *testing){
+func TestIsHappyFailure(t *testing.T){
   res := isHappy(2)
   if res {
 	  t.Errorf("Expected: false, Got: %v", res)
