dbf19ceaa4ee69a2e354f59f82e05b9629603878
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-09-07 02:01:02 +0530

    Remove files

--- Leetcode/LengthOfLastWord.go
+++
@@ -1,8 +0,0 @@
-package leetcode
-
-// https://leetcode.com/problems/length-of-last-word/submissions/
-
-func lengthOfLastWord(s string) int {
-	s3 := strings.Fields(strings.TrimSpace(s))
-	return len(s3[len(s3)-1])
-}
