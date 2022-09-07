f82474443a837b5c5b3c14ad003602f88d4b4370
Ankit Deshpande <ankitd009@users.noreply.github.com>
2022-09-07 17:32:11 +0530

    Update twoSum_test.go

--- Leetcode/twoSum_test.go
+++ Leetcode/twoSum_test.go
@@ -5,13 +5,13 @@ import "testing"
 func TestTwoSumSuccess(t *testing.T){
   result := twoSum([]int{3,3}, 6)
   if result != []int{0,1} {
-    t.Errof("Expected: %+v, Got: %+v", result)
+    t.ErrorF("Expected: %+v, Got: %+v", result)
   }  
 }
 
 func TestTwoSumSuccessII(t *testing.T){
   result := twoSum([]int{2,7, 11, 15}, 9)
   if result != []int{0,1} {
-    t.Errof("Expected: %+v, Got: %+v", result)
+    t.ErrorF("Expected: %+v, Got: %+v", result)
   }  
 }
